0464b3570eb2b8d14644d8391a379b235ada620e
ABDURAHIMhalilov <133384666+ABDURAHIMhalilov@users.noreply.github.com>
2023-07-04 12:47:37 +0500

    [
    [

--- App.js
+++ App.js
@@ -7,33 +7,38 @@ import {
   Alert,
   TouchableOpacity,
   SafeAreaView,
+  Button,
+  TextInput,
+  Platform,
+  StatusBar,
+  Dimensions,
 } from "react-native";
 // import Ionic from "react-native-vector-icons/Ionicons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigation } from "@react-navigation/bottom-tabs";
 import HomePage from "./app/HomeHeader";
 import SearchScreen from "./app/SearchScreen";
-import { Button, TextInput } from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
+
   return (
-    <SafeAreaView>
-      <View style={styles.contain}>
-        <Button
-        
-        />
-      </View>
+    <SafeAreaView style={styles.contain}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: "30%",
+        }}
+      ></View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   contain: {
-    width: 500,
-    height: 500,
-    backgroundColor: "red",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
